a6522290f4e77e3b2a39f1bac6785a79b336023b
Wirawat Jaiarree <katak6565@gmail.com>
2021-02-04 15:48:40 +0700

    [ADD] pass parameter function

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -13,17 +13,41 @@ class MyApp extends StatelessWidget {
         title: Text('Row and Col1'),
       ),
       body: Center(
-        child: buildRow(),
+        child: _buildImageColumn(),
       ),
     ));
   }
 
-  Widget buildRow() => Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+  Widget _buildImageColumn() => Container(
+        decoration: BoxDecoration(color: Colors.black26),
+        child: Column(
+          children: [_buildImageRow(1), _buildImageRow(3)],
+        ),
+      );
+
+  Widget _buildDecoratedImage(int imageIndex) => Expanded(
+        child: Container(
+          decoration: BoxDecoration(
+              border: Border.all(width: 10, color: Colors.black38),
+              borderRadius: const BorderRadius.all(const Radius.circular(8))),
+          margin: const EdgeInsets.all(4),
+          child: Image.asset('assets/images/pic$imageIndex.jpg'),
+        ),
+      );
+
+  Widget _buildImageRow(int imageIndex) => Row(
         children: [
-          Image.asset('assets/images/pic1.jpg'),
-          Image.asset('assets/images/pic2.jpg'),
-          Image.asset('assets/images/pic3.jpg'),
+          _buildDecoratedImage(imageIndex),
+          _buildDecoratedImage(imageIndex + 1)
         ],
       );
 }
+
+// Widget buildRow() => Row(
+//       mainAxisSize: MainAxisSize.min,
+//       children: [
+//         Image.asset('assets/images/pic1.jpg'),
+//         Image.asset('assets/images/pic2.jpg'),
+//         Image.asset('assets/images/pic3.jpg'),
+//       ],
+//     );
